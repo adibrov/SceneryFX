a54676abafd4feefdd5452dfd9357971606dd8eb
adibrov <alexander.dibroff@gmail.com>
2017-01-20 12:52:40 +0100

    highlights the label

--- src/main/java/sceneryfx/AcquisitonModel.java
+++ src/main/java/sceneryfx/AcquisitonModel.java
@@ -1,5 +1,6 @@
 package sceneryfx;
 
+import javafx.util.Pair;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgFactory;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
@@ -16,6 +17,8 @@ public class AcquisitonModel {
     private int mDimY;
     private int mDimZ;
     private Img mData;
+    private ArrayList<Rectangle>[] mGeometry;
+    private HashMap<Rectangle, AcquisitionUnit> mLabelRectangleMap;
 
     public ArrayList<Rectangle>[] getGeometry() {
         return mGeometry;
@@ -25,25 +28,24 @@ public class AcquisitonModel {
         return mData;
     }
 
-    private ArrayList<Rectangle>[] mGeometry;
-    private HashMap<Rectangle, AcquisitionUnit> mLabelRectangleMap;
 
     public HashMap<Rectangle, AcquisitionUnit> getLabelRectangleMap() {
         return mLabelRectangleMap;
     }
 
     public AcquisitonModel(int pDimX, int pDimY, int pDimZ) {
-        this.mData = new ArrayImgFactory().create(new long[] {pDimX, pDimY, pDimZ}, new UnsignedByteType());
+        this.mData = new ArrayImgFactory().create(new long[]{pDimX, pDimY, pDimZ}, new UnsignedByteType());
         this.mGeometry = new ArrayList[pDimZ];
         for (int i = 0; i < pDimZ; i++) {
 
             mGeometry[i] = new ArrayList<>();
+            mGeometry[i].add(new Rectangle(0,0,0,0));
         }
         this.mLabelRectangleMap = new HashMap<>();
     }
 
-    public void addAcquisitionUnitWithLabel(AcquisitionUnit pAcquisitionUnit, Rectangle pRectangleLabel, int []
-            pLocation, int plastLabelZ){
+    public void addAcquisitionUnitWithLabel(AcquisitionUnit pAcquisitionUnit, Rectangle pRectangleLabel, int[]
+            pLocation, int plastLabelZ) {
         mLabelRectangleMap.put(pRectangleLabel, pAcquisitionUnit);
         for (int i = pLocation[2]; i < plastLabelZ + 1; i++) {
             mGeometry[i].add(new Rectangle(pRectangleLabel));
@@ -52,12 +54,12 @@ public class AcquisitonModel {
     }
 
     public static void main(String[] args) {
-        AcquisitonModel lAcquisiitonModel = new AcquisitonModel(1000,1000,1000);
+        AcquisitonModel lAcquisiitonModel = new AcquisitonModel(1000, 1000, 1000);
         AcquisitionUnit lAcquisitionUnit1 = new AcquisitionUnit();
-        AcquisitionUnit lAcquisitionUnit2 = new AcquisitionUnit(new int[] {10,10,10});
+        AcquisitionUnit lAcquisitionUnit2 = new AcquisitionUnit(new int[]{10, 10, 10});
 
         lAcquisiitonModel.addAcquisitionUnitWithLabel(lAcquisitionUnit1, new Rectangle(lAcquisitionUnit1.getLocation()
-                [0], lAcquisitionUnit1.getLocation()[1], lAcquisitionUnit1.getDimX(), lAcquisitionUnit1.getDimY()),
+                        [0], lAcquisitionUnit1.getLocation()[1], lAcquisitionUnit1.getDimX(), lAcquisitionUnit1.getDimY()),
                 lAcquisitionUnit1.getLocation(),
                 lAcquisitionUnit1.getLocation()[2]);
 
--- src/main/java/sceneryfx/Viewer.java
+++ src/main/java/sceneryfx/Viewer.java
@@ -4,11 +4,16 @@ import bdv.util.Bdv;
 import bdv.util.BdvFunctions;
 import bdv.util.BdvOverlay;
 import clearvolume.renderer.cleargl.overlay.Overlay;
+import javafx.util.Pair;
 import org.scijava.ui.behaviour.io.InputTriggerConfig;
 import org.scijava.ui.behaviour.io.yaml.YamlConfigIO;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by dibrov on 12/01/17.
@@ -17,20 +22,57 @@ public class Viewer {
     private AcquisitonModel mAcquisitionModel;
     private Bdv bdv;
     private BdvOverlay mOverlay;
+    private Color mColor;
     private StackSelectionLabel mStackSelectionLabel;
+    private MouseMotionListener mMouseMotionListener;
 
     public Viewer(AcquisitonModel pAcquisitionModel){
         this.mAcquisitionModel = pAcquisitionModel;
+        this.mColor = new  Color(1.0f,153.0f/255, 51.0f/255, 0.5f);
+
+        this.mMouseMotionListener = new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                Rectangle rect = new Rectangle();
+                int lCurrentTimePoint = bdv.getBdvHandle().getViewerPanel().getState().getCurrentTimepoint();
+                ArrayList<Rectangle> lLabels = getAcquisitionModel().getGeometry()[lCurrentTimePoint];
+                Rectangle lRectToSelect = new Rectangle(0,0,0,0);
+                int lRectToSelectArea = 100000000;
+                for (int i = 1; i < lLabels.size(); i++){
+                    if (lLabels.get(i).contains(e.getX(), e.getY()) && lLabels.get(i).width*lLabels.get(i).height <
+                            lRectToSelectArea) {
+                        lRectToSelect = lLabels.get(i);
+                        lRectToSelectArea = lRectToSelect.height*lRectToSelect.width;
+                    }
+                }
+                lLabels.set(0,lRectToSelect);
+            }
+        };
 
 
         mOverlay = new BdvOverlay() {
             @Override
             protected void draw(Graphics2D g) {
-                int i = info.getTimePointIndex();
-                for (Rectangle rect: mAcquisitionModel.getGeometry()[i]) {
-                    g.setColor(new  Color(1.0f,153.0f/255, 51.0f/255, 0.5f));
-                    g.draw(rect);
-                    g.fill(rect);
+                int lCurrentZSlice = info.getTimePointIndex();
+                ArrayList<Rectangle> lRectArray =mAcquisitionModel.getGeometry()[lCurrentZSlice];
+                for (int i = 0; i< lRectArray.size(); i++) {
+                    if (i != 0) {
+                        g.setColor(mColor);
+                        g.draw(lRectArray.get(i));
+                        g.fill(lRectArray.get(i));
+                    }
+                    else{
+                        g.setColor(mColor);
+                        g.setStroke(new BasicStroke(4));
+                        g.draw(lRectArray.get(i));
+                    }
+
+
                 }
             }
         };
@@ -57,6 +99,10 @@ public class Viewer {
         this.bdv = BdvFunctions.show(mAcquisitionModel.getData(), "reds", Bdv.options().is2D().inputTriggerConfig
                 (conf));
         this.mStackSelectionLabel = new StackSelectionLabel(bdv.getBdvHandle());
+
+
+
+        bdv.getBdvHandle().getViewerPanel().getDisplay().addMouseMotionListener(mMouseMotionListener);
 
         BdvFunctions.showOverlay(mStackSelectionLabel.getOverlay(), "overlay", Bdv.options().addTo(bdv));
         BdvFunctions.showOverlay(mOverlay, "overlay", Bdv.options().addTo(bdv));
